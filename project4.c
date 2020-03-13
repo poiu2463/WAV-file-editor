1ae05f12a4ea079d5f46bd77d53892d7b6bbaade
Connor May <poiu2463@gmail.com>
2020-03-12 23:51:27 -0400

    Create project4.c

---
+++ project4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "wave.h" //wittman's header file (MUST BE COMPILED)
+
+int main (int argc, char** argv){
+	for(int i = 0; i < argc - 1; i ++){
+    
+  
+  }
+	
+	return 0;
+	
+}
