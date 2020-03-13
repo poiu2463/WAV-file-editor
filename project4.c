ad536ee7eaf88c80db856a94f774750232aff9e6
Connor May <poiu2463@gmail.com>
2020-03-13 13:37:37 -0400

    Added command line compare
    added command line compares and error message for invalid command line argument.

--- project4.c
+++ project4.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <stderr.h>
 #include <string.h>
 #include "wave.h" //wittman's header file (MUST BE COMPILED)
 
 int main (int argc, char** argv){
 	for(int i = 0; i < argc - 1; i ++){
-    
-  
+    		char [] currentFlag = "-r";
+		char [] currentArgV = argv [i];
+		
+		if(strcmp(currentArgV, currentFlag)){
+			//reverse sound here	
+		}else if(strcmp(currentArgv, (currentFlag = "-s"))){
+			//speed change here	
+		}else if(strcmp(currentArgv, (currentFlag = "-f"))){
+			//flip channels here
+		}else if(strcmp(currentArgv, (currentFlag = "-o"))){
+			//fade out here
+		}else if(strcmp(currentArgv, (currentFlag = "-i"))){
+			//fade in here
+		}else if(strcmp(currentArgv, (currentFlag = "-v"))){
+			//change volume here
+		}else if(strcmp(currentArgv, (currentFlag = "-e"))){
+			//echo here
+		}else{
+			//Command line error message here
+			fpringf("Usage: wave [[-r][-s factor][-f][-o delay][-i delay][-v scale][-e delay scale] < input > output\n");
+		}
+	}
   }
 	
 	return 0;
