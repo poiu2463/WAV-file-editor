4a7f801fc6ac14e7c272b4a3c4b3a6716ede447a
Connor May <poiu2463@gmail.com>
2020-03-26 10:28:12 -0400

    Update project4.c

--- project4.c
+++ project4.c
@@ -3,32 +3,59 @@
 #include <string.h>
 #include "wave.h" //wittman's header file (MUST BE COMPILED)
 
+
+/**
+ * Function: 	getShort (short)
+ * Returns: 	an int, read from standard input. 
+ **/
+
+short getShort() {
+  int firstByte = getchar();
+  int secondByte = getchar();
+
+  short temp = secondByte<<8 | firstByte;
+  return temp;
+}
+
 int main (int argc, char** argv){
-	for(int i = 0; i < argc - 1; i ++){
-    		char [] currentFlag = "-r";
-		char [] currentArgV = argv [i];
+  WaveHeader header;
+  readHeader(&header);
+  int length = header.dataChunk.size/4;
+
+  short* left = (short*)malloc(sizeof(short)*length);
+  short* right = (short*)malloc(sizeof(short)*length);
+
+  for(int i = 0; i < length; ++i){
+    left[i] = getShort();
+    right[i] = getShort();
+  }
+  
+	for(int i = 1; i < argc; ++i){
+		char* currentArgV = argv [i];
 		
-		if(strcmp(currentArgV, currentFlag)){
+		if(strcmp(currentArgV, "-r") == 0){
 			//reverse sound here	
-		}else if(strcmp(currentArgv, (currentFlag = "-s"))){
+		}else if(strcmp(currentArgv, "-s") == 0){
 			//speed change here	
-		}else if(strcmp(currentArgv, (currentFlag = "-f"))){
+		}else if(strcmp(currentArgv, "-f") == 0){
 			//flip channels here
-		}else if(strcmp(currentArgv, (currentFlag = "-o"))){
+		}else if(strcmp(currentArgv, "-o") == 0){
 			//fade out here
-		}else if(strcmp(currentArgv, (currentFlag = "-i"))){
+		}else if(strcmp(currentArgv, "-i") == 0){
 			//fade in here
-		}else if(strcmp(currentArgv, (currentFlag = "-v"))){
+		}else if(strcmp(currentArgv, "-v") == 0){
 			//change volume here
-		}else if(strcmp(currentArgv, (currentFlag = "-e"))){
+		}else if(strcmp(currentArgv, "-e") == 0){
 			//echo here
 		}else{
 			//Command line error message here
 			fprintf("Usage: wave [[-r][-s factor][-f][-o delay][-i delay][-v scale][-e delay scale] < input > output\n");
 		}
+
 	}
-  }
-	
+  //here we need to writeHeader
+  //output all left/right channel data by doing the opposite of the above input blocks of code: read two shorts in left and right channel and output the bytes 
+  
 	return 0;
 	
 }
