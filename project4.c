702628b45c7a7dcef3644d0db5fa10210bdecbed
Connor May <poiu2463@gmail.com>
2020-04-03 23:32:54 -0400

    Update project4.c

--- project4.c
+++ project4.c
@@ -13,7 +13,7 @@ void reverse (short* channel, int length); //prototype functions
 
 void fadeIn(short* channel, int length, double seconds);
 
-void fadeOut(double time);
+void fadeOut(short* channel, int length, double time);
 
 short* changeSpeed(short* channel, int length, int factor);
 //prototype for changing the speed
@@ -24,7 +24,7 @@ void volumeChange(short* left, short* right, double scale, int length);
 short clamp (double value);
 //prototype of clamp
 
-void echo(double delay, double scale);
+void echo(double delay, double scale, short* channel, int length);
 
 /**
  * Function:  getShort (short)
@@ -66,113 +66,134 @@ int main (int argc, char** argv){
     right[i] = getShort();
   }
   
-	for(int i = 1; i < argc; ++i){
-		char* currentArgV = argv [i];
-		
-		if(strcmp(currentArgV, "-r") == 0){
-			//reverse sound here	
-			fprintf(stderr, "Reversing\n");
-			reverse(left, length);
-			reverse(right, length);
-		}else if(strcmp(currentArgV, "-s") == 0){
-			//speed change here	
-			++i;
-			double factor = atof(argv[i]);
-			left = changeSpeed(left, length, factor);
-			right = changeSpeed(right, length, factor);
+for(int i = 1; i < argc; ++i){
+char* currentArgV = argv [i];
 
-			length = length/factor;
-			
-		}else if(strcmp(currentArgV, "-f") == 0){
-			//flip channels here
-			short* temp = right;
-  			right = left;
-  			left = temp;
-		}else if(strcmp(currentArgV, "-o") == 0){
-			//fade out here
-		}else if(strcmp(currentArgV, "-i") == 0){
-			//fade in here
-		}else if(strcmp(currentArgV, "-v") == 0){
-			//change volume here
-			++i;
-			double scale = atof(argv[i]);
-			volumeChange(left, right, scale, length);
-			
-		}else if(strcmp(currentArgV, "-e") == 0){
-			//echo here
-		}else{
-			//Command line error message here
-			fprintf(stderr, "Usage: wave [[-r][-s factor][-f][-o delay][-i delay][-v scale][-e delay scale] < input > output\n");
-		}
+if(strcmp(currentArgV, "-r") == 0){
+//reverse sound here 
+fprintf(stderr, "Reversing\n");
+reverse(left, length);
+reverse(right, length);
+}else if(strcmp(currentArgV, "-s") == 0){
+//speed change here 
+++i;
+double factor = atof(argv[i]);
+left = changeSpeed(left, length, factor);
+right = changeSpeed(right, length, factor);
 
-	}
-	header.dataChunk.size = length * 4;
-	header.size = header.dataChunk.size + 36;
-  //here we need to writeHeader
-  //output all left/right channel data by doing the opposite of the above input blocks of code: read two shorts in left and right channel and output the bytes 
-	writeHeader(&header);
+length = length/factor;
 
-	for(int i = 0; i < length; ++i){
-    	writeBytes(left[i]);
-    	writeBytes(right[i]);
-  	}
-	return 0;
-	
+}else if(strcmp(currentArgV, "-f") == 0){
+//flip channels here
+short* temp = right;
+  right = left;
+  left = temp;
+}else if(strcmp(currentArgV, "-o") == 0){
+//fade out here
+}else if(strcmp(currentArgV, "-i") == 0){
+//fade in here
+++i;
+double seconds = atof(argv[i]);
+fadeIn(left, length, seconds);
+fadeIn(right, length, seconds);
+
+}else if(strcmp(currentArgV, "-v") == 0){
+//change volume here
+++i;
+double scale = atof(argv[i]);
+volumeChange(left, right, scale, length);
+
+}else if(strcmp(currentArgV, "-e") == 0){
+//echo(delay, scale, channel, length);
+//echo here
+}else{
+//Command line error message here
+fprintf(stderr, "Usage: wave [[-r][-s factor][-f][-o delay][-i delay][-v scale][-e delay scale] < input > output\n");
 }
 
-	void reverse (short* channel, int length) {
-		for(int i = 0; i < length/2; i++){ // divide by 4 to get half of each channel
-			short temp = channel[length-1-i];
-			channel[length-1-i] = channel[i];
-			channel[i] = temp;	
-		}
-	}
+}
+header.dataChunk.size = length * 4;
+header.size = header.dataChunk.size + 36;
+  //here we need to writeHeader
+  //output all left/right channel data by doing the opposite of the above input blocks of code: read two shorts in left and right channel and output the bytes 
+writeHeader(&header);
 
-	short* changeSpeed(short* channel, int length, int factor){
-	  short* newChannel = (short*)malloc(sizeof(short)*length/factor);
+for(int i = 0; i < length; ++i){
+    writeBytes(left[i]);
+    writeBytes(right[i]);
+  }
+return 0;
 
-	  for(int i = 0; i < length/factor; i++){
-		newChannel[i] = channel[i*factor];
-	  }
-	  free(channel);
-	  return newChannel;
-  	}
+}
 
-	void fadeIn(short* channel, int length, double seconds) {
-		int sampleNumber = SAMPLE_RATE * seconds;
-		for(int i = 0; i < sampleNumber && i < length; ++i) {
-			channel[i] *= (i/(double)sampleNumber)*(i/(double)sampleNumber);
-		}
-	}
+void reverse (short* channel, int length) {
+for(int i = 0; i < length/2; i++){ // divide by 4 to get half of each channel
+short temp = channel[length-1-i];
+channel[length-1-i] = channel[i];
+channel[i] = temp; 
+}
+}
 
-	void fadeOut(double time) {
-		int sampleNumber = SAMPLE_RATE * time;
-	}
+short* changeSpeed(short* channel, int length, int factor){
+ short* newChannel = (short*)malloc(sizeof(short)*length/factor);
+
+ for(int i = 0; i < length/factor; i++){
+newChannel[i] = channel[i*factor];
+ }
+ free(channel);
+ return newChannel;
+  }
+
+void fadeIn(short* channel, int length, double seconds) {
+int sampleNumber = SAMPLE_RATE * seconds;
+for(int i = 0; i < sampleNumber && i < length; ++i) {
+channel[i] *= (i/(double)sampleNumber)*(i/(double)sampleNumber);
+}
+}
+
+void fadeOut(short* channel, int length, double time) {
+int sampleNumber = SAMPLE_RATE * time;
+for(int i = 0; i < sampleNumber && i >= time; ++i) {
+channel[i] *= (i/(double)sampleNumber)*(i/(double)sampleNumber);
+
+} 
+}
   
-	  
-	void volumeChange(short* left, short* right, double scale, int length){
-		for(int i = 0; i < length; i++){
-			int leftNum = left[i];
-			int rightNum = right[i];
-			leftNum *= scale;
-			rightNum *= scale;
-			left[i] = clamp(leftNum);
-			right[i] = clamp(rightNum);
-		}
-	}
-	
-	short clamp (double value){
-		if(value < SHRT_MIN)
-			return (short) SHRT_MIN;
-		if(value > SHRT_MAX)
-			return (short) SHRT_MAX;
-		return (short) value;
-	}
+ 
+void volumeChange(short* left, short* right, double scale, int length){
+for(int i = 0; i < length; i++){
+int leftNum = left[i];
+int rightNum = right[i];
+leftNum *= scale;
+rightNum *= scale;
+left[i] = clamp(leftNum);
+right[i] = clamp(rightNum);
+}
+}
 
-	// void echo(double delay, double scale){
-	// 	int sampleNumber = delay * SAMPLE_RATE;
-	// 	short* newRightSound = (short*)malloc(sizeof(short)*sampleNumber);
-	// 	short* newLeftSound = (short*)malloc(sizeof(short)*sampleNumber);
-	// 	while()
-	// }
+short clamp (double value){
+if(value < SHRT_MIN)
+return (short) SHRT_MIN;
+if(value > SHRT_MAX)
+return (short) SHRT_MAX;
+return (short) value;
+}
+
+void echo(double delay, double scale, short* channel, int length){
+int sampleNumber = delay * SAMPLE_RATE;
+short* newRightSound = (short*)malloc(sizeof(short)*sampleNumber);
+short* newLeftSound = (short*)malloc(sizeof(short)*sampleNumber);
+
+int i =0;
+while(i < sizeof(short)*sampleNumber){
+if( i < delay){
+newRightSound[i] = channel[i];
+newLeftSound[i] = channel[i];
+}
+else{
+newRightSound[i] = (channel[i]+delay)*scale;
+newLeftSound[i] = (channel[i]+delay)*scale;
+} 
+}
+}
 
